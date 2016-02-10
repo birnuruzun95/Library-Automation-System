1cd49396479376625cfdd72b98311a6ae74fd3b9
birnuruzun95 <birnuruzun.294976@gmail.com>
2016-02-10 13:23:40 +0200

    projenin base kısmı oluşturuldu

--- book_automation_project/src/book_automation_project/Books.java
+++ book_automation_project/src/book_automation_project/Books.java
@@ -5,43 +5,57 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-public class Books implements Serializable{
-	private String writer,book_name;
+public class Books implements Serializable {
+	private String writer, book_name;
 	private int isbn;
-	
+
 	private List<VIS> vis;
-	
-	public Books(){
+
+	public Books() {
 		isbn = 0;
 		writer = null;
 		book_name = null;
-		vis =new ArrayList<VIS>();
+		vis = new ArrayList<VIS>();
 	}
-	public Books(String writer,String book_name,int isbn){
+
+	public Books(String writer, String book_name, int isbn) {
 		this.isbn = isbn;
 		this.writer = writer;
-		this.book_name = book_name;				
+		this.book_name = book_name;
 	}
-	public void addVIS(VIS v){
+
+	public void addVIS(VIS v) {
 		vis.add(v);
 	}
-	public String getBook_name(){
+
+	public String getBook_name() {
 		return book_name;
 	}
-	public VIS getVISByName(String name){
+
+	public VIS getVISByName(String name) {
 		VIS v = null;
-		Iterator<VIS> i =vis.iterator();
-		while(i.hasNext()){
+		Iterator<VIS> i = vis.iterator();
+		while (i.hasNext()) {
 			v = i.next();
-			if(v.getName().toLowerCase().contentEquals(name.toLowerCase())){
+			if (v.getName().toLowerCase().contentEquals(name.toLowerCase())) {
 				return v;
 			}
 		}
-		return null; 		
+		return null;
 	}
+
 	@Override
-	public String toString(){
-		return "\nBook's Name:"+ book_name+"\nWriter:"+writer+"\nisbn no:"+isbn+"\n";
+	public String toString() {
+		String visName = " ";
+		String visAmount = "(" + String.valueOf(vis.size()) + ")";
+
+		Iterator<VIS> i = vis.iterator();
+		while (i.hasNext()) {
+			visName += i.next().getName() + ",";
+		}
+
+		return "\nBook's Name:" + book_name + "\nWriter:" + writer + 
+				"\nisbn no:" + isbn + "\nVIS files" + visAmount + ":" + visName+ "\n";
 	}
-	
+
 }
--- book_automation_project/src/book_automation_project/MainSystem.java
+++ book_automation_project/src/book_automation_project/MainSystem.java
@@ -1,5 +1,6 @@
 package book_automation_project;
 
+import java.awt.Desktop;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -22,36 +23,81 @@ public class MainSystem {
 
 	public static void main(String[] args) {
 		while (running) {
-			System.out.println("\nEnter 0 for load a library." 
-		                      + "\nEnter 1 for save and quit"
-					          + "\nEnter 2 for list all books in library" 
-					          + "\nEnter 3 for add book to library"
-					          + "\nEnter 4 for add video/image to archive"
-					          + "\nEnter 5 for load and export video/image/sounds file from archive and play it");
+			System.out.println("\nEnter 0 for load a library." + "\nEnter 1 for save and quit"
+					+ "\nEnter 2 for list all books in library" + "\nEnter 3 for add book to library"
+					+ "\nEnter 4 for add video/image to archive"
+					+ "\nEnter 5 for load and export video/image/sounds file from archive and play it");
 			int answer = in.nextInt();
 			switch (answer) {
 			case 0:
-				System.out.println("enter the file name to load!");				
+				System.out.println("enter the file name to load!");
 				loadscript(in.next());
 				break;
 			case 1:
 				saveAndQuit();
 				break;
 			case 2:
-				System.out.println(lib.toString());				
+				System.out.println(lib.toString());
 				break;
 			case 3:
 				addBook();
 				break;
 			case 4:
-				addVIS();
+				importOpenAndSave();
 				break;
 			case 5:
-				
+				loadOpenAndExport();
 				break;
 			}
 		}
 		System.exit(0);
+	}
+
+	private static void importOpenAndSave() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void loadOpenAndExport() {
+		// TODO Auto-generated method stub
+		String visName = null;
+		String bookName = null;
+		Books book;
+		VIS vis = null;
+		byte[] data = null;
+		File file;
+		FileOutputStream out = null;
+		
+		System.out.println("\nEnter book name:");
+		bookName = in.next();
+		book = lib.getBookByName(bookName);
+		if(book== null){
+			System.out.println("book does not exist!");
+		}else{
+			System.out.println(book.toString());
+			System.out.println("Enter the complete file name:");
+			visName = in.next();
+			vis = book.getVISByName(visName);
+			if(vis == null){
+				System.out.println("File does not exist!");
+			}else{
+				data = vis.getData();
+				file = new File(vis.getName());
+				try {
+					out = new FileOutputStream(file);
+					out.write(data);
+					out.close();
+					Desktop.getDesktop().open(file);
+					
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 
 	private static void addVIS() {
@@ -59,75 +105,92 @@ public class MainSystem {
 		JFileChooser chooser;
 		FileFilter filter;
 		FileInputStream fis = null;
-		
+
 		Books book;
 		String bookName;
 		VIS vis;
 		File file = null;
 		byte[] data = null;
-		
+
 		boolean stop = false;
-		
+
 		System.out.println("\nEnter book name to put on the file on:");
 		bookName = in.next();
 		book = lib.getBookByName(bookName);
-		if (book == null){
+		if (book == null) {
 			System.out.println("book does not exist!");
-		}else{
+		} else {
 			System.out.println("\nChoose your video/image/sound file to and:");
 			try {
-				Thread.sleep(1000);// kullanýcý bu sýrada prompt edebilir diye.//1 sn(1000 ms)
+				Thread.sleep(1000);// kullanýcý bu sýrada prompt edebilir
+									// diye.//1 sn(1000 ms)
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			} 
+			}
 			chooser = new JFileChooser();
-			filter = new FileNameExtensionFilter("Video/Image/Sounds Files","wav","mp3","avi","mp4","png","jpeg");
-		
+			filter = new FileNameExtensionFilter("Video/Image/Sounds Files", "wav", "mp3", "avi", "mp4", "png", "jpeg");
+
 			chooser.addChoosableFileFilter(filter);
 			chooser.setFileFilter(filter);
 			int resultCode = chooser.showOpenDialog(null);
-			if(resultCode == JFileChooser.APPROVE_OPTION){
-				//kullanýcý çift click ile dosya seçebilsin diye APPROVE_OPTIOn kullandým
+			if (resultCode == JFileChooser.APPROVE_OPTION) {
+				// kullanýcý çift click ile dosya seçebilsin diye APPROVE_OPTIOn
+				// kullandým
 				file = chooser.getSelectedFile();
-				data = new byte[(int)file.length()];//kodu bloklamýþ olduk
-			}else{
+				data = new byte[(int) file.length()];// kodu bloklamýþ olduk.
+														// break komutunda bu
+														// kýsma gelecek
+
+			} else {
 				System.out.println("you cancelled adding a VIS");
 				stop = true;
 			}
-			if(!stop){
-				fis = new FileInputStream(file);
-				fis.read(data);
-				fis close();
-				
+			if (!stop) {
+				try {
+					fis = new FileInputStream(file);
+					fis.read(data);
+					fis.close();
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				vis = new VIS(file.getName(), data);
+				book.addVIS(vis);
+
+				System.out.println("You have added" + file.getName()+
+						"to book" + bookName + "\n");
 			}
 		}
 	}
 
 	private static void addBook() {
 		// TODO Auto-generated method stub
-	    String writer,book_name;
+		String writer, book_name;
 		int isbn;
-		
+
 		System.out.println("\nEnter book name:");
 		book_name = in.next();
 		System.out.println("\nEnter writer:");
 		writer = in.next();
 		System.out.println("\nEnter ISBN:");
 		isbn = in.nextInt();
-			
-		Books b = new Books(book_name,writer,isbn);
+
+		Books b = new Books(book_name, writer, isbn);
 		lib.addBook(b);
 	}
 
 	private static void saveAndQuit() {
 		// TODO Auto-generated method stub
 		System.out.println("Enter file name:");
-		fileName= in.next() + ".ser";
+		fileName = in.next() + ".ser";
 		running = false;
 		FileOutputStream fos = null;
 		ObjectOutputStream out = null;
-		
+
 		try {
 			fos = new FileOutputStream(fileName);
 			out = new ObjectOutputStream(fos);
@@ -137,7 +200,7 @@ public class MainSystem {
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (IOException e) {			
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -145,28 +208,28 @@ public class MainSystem {
 
 	private static void loadscript(String fileName2) {
 		// TODO Auto-generated method stub
-	
+
 		FileInputStream fis = null;
 		ObjectInputStream in = null;
-		
-		File file = new File (fileName);
-		if (file.exists()){
-		try {
-			fis = new FileInputStream(fileName + ".ser");
-			in = new ObjectInputStream(fis);
-			lib = (Library)in.readObject();	
-			fis.close();
-			in.close();				 
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		}else{
+
+		File file = new File(fileName);
+		if (file.exists()) {
+			try {
+				fis = new FileInputStream(fileName + ".ser");
+				in = new ObjectInputStream(fis);
+				lib = (Library) in.readObject();
+				fis.close();
+				in.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
 			System.out.println("\nThe file does not exist!");
 		}
 	}
-	
+
 }
